787d4e89d44768ed57e542b2688e4cd7af56b06b
gust9988 <68531770+gust9988@users.noreply.github.com>
2020-07-31 17:26:06 +0900

    Add files via upload

---
+++ team-akatsuki/-NOTICE-/200731_answer_fruitshop.py
@@ -0,0 +1,50 @@
+import os
+
+report_data = []
+# 데이터 저장
+
+def dir_list(param_string):
+    with os.scandir(param_string) as entries:
+        for entry in entries:
+            file_name = entry.name
+            print(file_name)
+            t_list = transform_data("{0}\\{1}".format(param_string,file_name))
+            make_report(t_list)
+
+def transform_data(param):
+    data_list = []
+    with open(param, 'r', encoding='utf8') as f:
+        line1 = f.readline()
+        line2 = f.readline()
+        print(line2)
+        temp_list = line2.split(",")
+        length = len(temp_list)
+        length = int(length / 2)
+        for i in range(length):
+            data_list.append([temp_list[i * 2], int(temp_list[i * 2 + 1])])
+    print(data_list)
+    return data_list
+
+def check_dupl(item_list):
+    # 중복여부, 중복인덱스
+    for i in range(len(report_data)):
+        if report_data[i][0] == item_list[0]:
+            return True, i
+    return False, None
+
+def make_report(param_list):
+    # [['a', 1], ['b', 1], ['a', 1], ['c', 1]]
+    for i in param_list:
+        # ['a', 1]
+        is_dupl, idx_dupl  = check_dupl(i)
+        if is_dupl:
+            report_data[idx_dupl][1] = i[1] + report_data[idx_dupl][1]
+        else:
+            report_data.append(i)
+
+    # csv 출력 형식으로 파일을 저장한다.
+    with open('202007.csv', 'w', encoding='utf8') as f:
+        for i in report_data:
+            f.write("{0},{1}\n".format(i[0],i[1]))
+
+dir_list('202007')
