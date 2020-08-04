619a0ac6c966ef4104f1021115fda2ec9b1b372d
gust9988 <68531770+gust9988@users.noreply.github.com>
2020-08-04 14:12:03 +0900

    된다 ㅅㅂ
    IT WORKS!!!!!!!!!!!!!

--- team-akatsuki/kimgh/200804_fruitshop_kimgh.py
+++ team-akatsuki/kimgh/200804_fruitshop_kimgh.py
@@ -1,30 +1,49 @@
 import os
 
 
-def sale_scan(param):
-    with os.scandir(param) as entries:
-        for entry in entries:
-            sale_file = entry.name
-            sale_list = sale_dic("{}\\{}".format(param,sale_file))
-    return sale_file
+################################################################
 
-def sale_dic(param):
+def sale_sort(param):
+    param = param.split(",")
+    dic = {}
+    for i in range(len(param)):
+        if i % 2 == 0:
+            if not param[i] in dic:
+                dic[param[i]] = int(param[i + 1])
+            else:
+                dic[param[i]] += int(param[i + 1])
+    return dic
+
+
+################################################################
+
+
+def sale_trans(param):
     with open(param, 'r', encoding='utf8') as f:
         line1 = f.readline()
         line2 = f.readline()
-        line2 = line2.split(",")
-        dic = {}
-        for i in range(len(line2)):
-            if i % 2 == 0:
-                if not line2[i] in dic:
-                    dic[line2[i]] = int(line2[i + 1])
-                else:
-                    dic[line2[i]] += int(line2[i + 1])
-        return dic
 
-def make_file(param):
-    with open('sale_list.csv','w',encoding='utf8') as f:
-        for i in dic:
-            f.write("{},{}\n".format(i[0],i[1]))
+    return sale_sort(line2)
 
-sale_dic(sale_scan("sale_list\\"))
+
+################################################################
+
+
+def sale_file(param):
+    with open("sale.csv", "w", encoding="utf8") as f:
+        for i in param:
+            f.write("{},{}\n".format(i, param.get(i)))
+
+
+################################################################
+
+
+def sale_finish(param):
+    with os.scandir(param) as entries:
+        for entry in entries:
+            file_name = entry.name
+            sale_list = sale_trans("{}\\{}".format(param, file_name))
+    return sale_list
+################################################################
+
+sale_file(sale_finish("sale_box"))
