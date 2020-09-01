20a4d2504e707c3d1ed8b33ec4fc53999580a234
aria9860 <aria9860@naver.com>
2020-09-01 16:34:39 +0900

    수정

--- team-akatsuki/moonhj/pygame_1_3.py
+++ team-akatsuki/moonhj/pygame_1_3.py
@@ -11,61 +11,66 @@ PINK2 = (255, 192, 203)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-font = pygame.font.Font('NanumPen.ttf', 25)
-font2 = pygame.font.Font('NanumPen.ttf', 60)
-font3 = pygame.font.Font('NanumPen.ttf', 22)
+font = pygame.font.Font('BMJUA_ttf.ttf', 25)
+font2 = pygame.font.Font('BMJUA_ttf.ttf', 60)
+font3 = pygame.font.Font('BMJUA_ttf.ttf', 22)
 
-# select_m = ['한식', '일식', '중식', '양식', '기타']
-# k_food = ['비빔밥', '제육덮밥', '김치찌개', '된장찌개', '냉면', '돼지불백', '칼국수', '수제비', '부대찌개', '고등어조림', '보쌈',
-#           '뼈해장국', '순대국']
-# j_food = ['초밥', '우동', '스끼야끼', '냉모밀', '냉우동', '도미조림', '규동', '가츠동', '오니기리', '라멘', '오야꼬동']
-# c_food = ['짜장면', '짬뽕', '탕수육', '마파두부', '깐풍기', '라조기', '마라탕', '마라샹궈', '고추잡채', '볶음밥', '양장피', '텐동']
-# w_food = ['오믈렛', '오므라이스', '스테이크', '햄버거', '치킨커틀릿', '포크커틀릿', '까르보나라', '비프스튜', '토마토파스타', '바베큐폭립']
-# etc_food = ['순대', '떡볶이', '김밥', '라면', '쌀국수', '분짜', '인도커리', '팟타이', '슈바인학센', '다이어트는 어때...?', '삼각김밥',
-#             '편의점 도시락']
-#
-#
-# def RandLunch(i):
-#     ko = random.sample(k_food, 5)
-#     ja = random.sample(j_food, 5)
-#     ch = random.sample(c_food, 5)
-#     we = random.sample(w_food, 5)
-#     etc = random.sample(etc_food, 5)
-#
-#     if i == 1:
-#         return ko
-#
-#     elif '일식' == menu:
-#         break
-#
-#     elif '중식' == menu:
-#         break
-#
-#     elif '양식' == menu:
-#         break
-#
-#     elif '기타' == menu:
-#         break
-#
-#
-# def ReTry():
-#     while True:
-#         retry = input('******************************************************************\n'
-#                       '다시 뽑으시겠습니까? 예/아니오 : ')
-#         if '예' == retry:
-#             RandLunch()
-#
-#         elif '아니오' == retry:
-#             print("프로그램을 종료합니다.")
-#             break
-#
-#         else:
-#             print('잘못 입력하셨습니다. 다시 입력해 주세요')
-#
-#
-# RandLunch()
+select_m = ['한식', '일식', '중식', '양식', '기타']
+k_food = ['비빔밥', '제육덮밥', '김치찌개', '된장찌개', '냉면', '돼지불백', '칼국수', '수제비', '부대찌개', '고등어조림', '보쌈',
+          '뼈해장국', '순대국']
+j_food = ['초밥', '우동', '스끼야끼', '냉모밀', '냉우동', '도미조림', '규동', '가츠동', '오니기리', '라멘', '오야꼬동']
+c_food = ['짜장면', '짬뽕', '탕수육', '마파두부', '깐풍기', '라조기', '마라탕', '마라샹궈', '고추잡채', '볶음밥', '양장피', '텐동']
+w_food = ['오믈렛', '오므라이스', '스테이크', '햄버거', '치킨커틀릿', '포크커틀릿', '까르보나라', '비프스튜', '토마토파스타', '바베큐폭립']
+etc_food = ['순대', '떡볶이', '김밥', '라면', '쌀국수', '분짜', '인도커리', '팟타이', '슈바인학센', '다이어트는 어때...?', '삼각김밥',
+            '편의점 도시락']
+
+
+def RandLunch(i):
+    ko = random.sample(k_food, 5)
+    ja = random.sample(j_food, 5)
+    ch = random.sample(c_food, 5)
+    we = random.sample(w_food, 5)
+    etc = random.sample(etc_food, 5)
+
+    if i == 1:
+        return ko
+    #
+    # elif i == menu:
+    #     break
+    #
+    # elif '중식' == menu:
+    #     break
+    #
+    # elif '양식' == menu:
+    #     break
+    #
+    # elif '기타' == menu:
+    #     break
+    #
+
+def ReTry():
+    while True:
+        retry = input('******************************************************************\n'
+                      '다시 뽑으시겠습니까? 예/아니오 : ')
+        if '예' == retry:
+            RandLunch()
+
+        elif '아니오' == retry:
+            print("프로그램을 종료합니다.")
+            break
+
+        else:
+            print('잘못 입력하셨습니다. 다시 입력해 주세요')
+
+
+RandLunch(0)
 # ReTry()
 def sub():
+    global KOREAN_BOX, KOREAN_TEXT, JAPANESE_BOX, JAPANESE_TEXT, CHINESE_BOX, CHINESE_TEXT
+    global WESTERN_BOX, WESTERN_TEXT, ETC_BOX, ETC_TEXT, RANDOM_SURFACE, RANDOM_RECT
+    global MAINTEXT_SURFACE, MAINTEXT_RECT, SUBTEXT_SURFACE, SUBTEXT_RECT, PUSHTEXT_SURFACE, PUSHTEXT_RECT
+    global KOREAN_SURFACE, JAPANESE_SURFACE, CHINESE_SURFACE, WESTERN_SURFACE, ETC_SURFACE
+    global KOREAN_RECT
     RANDOM_SURFACE = font.render('Random', True, PINK)
     RANDOM_RECT = RANDOM_SURFACE.get_rect(center=(100, 55))
 
@@ -78,27 +83,23 @@ def sub():
     PUSHTEXT_SURFACE = font3.render('하나를 눌러주세요', True, BLACK)
     PUSHTEXT_RECT = PUSHTEXT_SURFACE.get_rect(center=(250, 175))
 
-    KOREAN_BOX = pygame.draw.rect(DISPLAY, PINK, (75, 230, 350, 40))
+
     KOREAN_SURFACE = font.render('한식', True, WHITE)
     KOREAN_TEXT = KOREAN_SURFACE.get_rect(center=(250, 250))
 
 
-    JAPANESE_BOX = pygame.draw.rect(DISPLAY, PINK, (75, 300, 350, 40))
     JAPANESE_SURFACE = font.render("일식", True, WHITE)
     JAPANESE_TEXT = JAPANESE_SURFACE.get_rect(center=(250, 320))
 
 
-    CHINESE_BOX = pygame.draw.rect(DISPLAY, PINK, (75, 370, 350, 40))
     CHINESE_SURFACE = font.render('중식', True, WHITE)
     CHINESE_TEXT = CHINESE_SURFACE.get_rect(center=(250, 390))
 
 
-    WESTERN_BOX = pygame.draw.rect(DISPLAY, PINK, (75, 440, 350, 40))
     WESTERN_SURFACE = font.render('양식', True, WHITE)
     WESTERN_TEXT = WESTERN_SURFACE.get_rect(center=(250, 460))
 
 
-    ETC_BOX = pygame.draw.rect(DISPLAY, PINK, (75, 510, 350, 40))
     ETC_SURFACE = font.render('기타', True, WHITE)
     ETC_TEXT = ETC_SURFACE.get_rect(center=(250, 530))
 
@@ -110,21 +111,10 @@ def main():
     global WESTERN_BOX, WESTERN_TEXT, ETC_BOX, ETC_TEXT, RANDOM_SURFACE, RANDOM_RECT
     global MAINTEXT_SURFACE, MAINTEXT_RECT, SUBTEXT_SURFACE, SUBTEXT_RECT, PUSHTEXT_SURFACE, PUSHTEXT_RECT
     global KOREAN_SURFACE, JAPANESE_SURFACE, CHINESE_SURFACE, WESTERN_SURFACE, ETC_SURFACE
-
+    global KOREAN_RECT
     while True:
-        DISPLAY.blit(KOREAN_SURFACE, KOREAN_TEXT)
-        DISPLAY.blit(JAPANESE_SURFACE, JAPANESE_TEXT)
-        DISPLAY.blit(CHINESE_SURFACE, CHINESE_TEXT)
-        DISPLAY.blit(WESTERN_SURFACE, WESTERN_TEXT)
-        DISPLAY.blit(ETC_SURFACE, ETC_TEXT)
-        DISPLAY.fill(WHITE)  # 메인 화면
-        DISPLAY.blit(RANDOM_SURFACE, RANDOM_RECT)  # 메인 화면 텍스트(Random)
-        DISPLAY.blit(MAINTEXT_SURFACE, MAINTEXT_RECT)  # 메인 화면 텍스트(점심 메뉴 뽑기!)
-        DISPLAY.blit(SUBTEXT_SURFACE, SUBTEXT_RECT)  # 메인 화면 텍스트(아래의 메뉴 중)
-        DISPLAY.blit(PUSHTEXT_SURFACE, PUSHTEXT_RECT)  # 메인 화면 텍스트(하나를 눌러주세요)
-        pygame.draw.rect(DISPLAY, PINK, (42, 5, 415, 600), 3)  # 메인화면 프레임
-
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
@@ -137,7 +127,7 @@ def main():
                 else:
                     pygame.draw.rect(DISPLAY, PINK, KOREAN_BOX)
                     KOREAN_SURFACE = font.render('한식', True, WHITE)
-                DISPLAY.blit(KOREAN_SURFACE, KOREAN_TEXT)
+
 
                 if JAPANESE_BOX.collidepoint(event.pos):
                     pygame.draw.rect(DISPLAY, PINK2, JAPANESE_BOX)
@@ -145,7 +135,7 @@ def main():
                 else:
                     pygame.draw.rect(DISPLAY,PINK,JAPANESE_BOX)
                     JAPANESE_SURFACE = font.render('일식', True, WHITE)
-                DISPLAY.blit(JAPANESE_SURFACE, JAPANESE_TEXT)
+
 
                 if CHINESE_BOX.collidepoint(event.pos):
                     pygame.draw.rect(DISPLAY, PINK2, CHINESE_BOX)
@@ -153,7 +143,7 @@ def main():
                 else:
                     pygame.draw.rect(DISPLAY, PINK, CHINESE_BOX)
                     CHINESE_SURFACE = font.render('중식', True, WHITE)
-                DISPLAY.blit(CHINESE_SURFACE, CHINESE_TEXT)
+
 
                 if WESTERN_BOX.collidepoint(event.pos):
                     pygame.draw.rect(DISPLAY, PINK2, WESTERN_BOX)
@@ -161,7 +151,7 @@ def main():
                 else:
                     pygame.draw.rect(DISPLAY, PINK, WESTERN_BOX)
                     WESTERN_SURFACE = font.render('양식', True, WHITE)
-                DISPLAY.blit(WESTERN_SURFACE, WESTERN_TEXT)
+
 
                 if ETC_BOX.collidepoint(event.pos):
                     pygame.draw.rect(DISPLAY, PINK2, ETC_BOX)
@@ -169,7 +159,7 @@ def main():
                 else:
                     pygame.draw.rect(DISPLAY, PINK, ETC_BOX)
                     ETC_SURFACE = font.render('기타', True, WHITE)
-                DISPLAY.blit(ETC_SURFACE, ETC_TEXT)
+                    DISPLAY.blit(ETC_SURFACE, ETC_TEXT)
 
 
             if event.type == pygame.MOUSEBUTTONUP:
@@ -188,8 +178,28 @@ def main():
 
                 elif ETC_BOX.collidepoint(event.pos):
                     pass
+        DISPLAY.fill(WHITE)  # 메인 화면
 
-            pygame.display.update()
+        KOREAN_BOX = pygame.draw.rect(DISPLAY, PINK, (75, 230, 350, 40))
+        JAPANESE_BOX = pygame.draw.rect(DISPLAY, PINK, (75, 300, 350, 40))
+        CHINESE_BOX = pygame.draw.rect(DISPLAY, PINK, (75, 370, 350, 40))
+        WESTERN_BOX = pygame.draw.rect(DISPLAY, PINK, (75, 440, 350, 40))
+        ETC_BOX = pygame.draw.rect(DISPLAY, PINK, (75, 510, 350, 40))
+
+
+        DISPLAY.blit(KOREAN_SURFACE, KOREAN_TEXT)
+        DISPLAY.blit(JAPANESE_SURFACE, JAPANESE_TEXT)
+        DISPLAY.blit(CHINESE_SURFACE, CHINESE_TEXT)
+        DISPLAY.blit(WESTERN_SURFACE, WESTERN_TEXT)
+        DISPLAY.blit(ETC_SURFACE, ETC_TEXT)
+
+        DISPLAY.blit(RANDOM_SURFACE, RANDOM_RECT)  # 메인 화면 텍스트(Random)
+        DISPLAY.blit(MAINTEXT_SURFACE, MAINTEXT_RECT)  # 메인 화면 텍스트(점심 메뉴 뽑기!)
+        DISPLAY.blit(SUBTEXT_SURFACE, SUBTEXT_RECT)  # 메인 화면 텍스트(아래의 메뉴 중)
+        DISPLAY.blit(PUSHTEXT_SURFACE, PUSHTEXT_RECT)  # 메인 화면 텍스트(하나를 눌러주세요)
+        pygame.draw.rect(DISPLAY, PINK, (42, 5, 415, 600), 3)  # 메인화면 프레임
+
+        pygame.display.update()
 
 if __name__ == '__main__':
     main()
